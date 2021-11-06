4dc2ef4f24eaf1aad59313512b7465ca8f0246f9
Johny Castro <johny.castro.florez@gmail.com>
2021-11-06 11:06:05 -0500

    Versión 1.0.0 de la aplicación pedidos videos mintic

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,57 @@
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
+import {Persona} from './persona.model';
+import {Producto} from './producto.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_producto: string;
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estado: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_persona: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @hasOne(() => Producto)
+  productoId: Producto;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
